65399cb4badc671bbfcf0b439f26c973b712d718
ShiYing <shiyingxixi@163.com>
2024-03-29 17:27:22 +0800

    1

--- test.sh
+++ test.sh
@@ -19,7 +19,8 @@ echo "===== Jmeter 测试 ====="
 
 echo "创建日志目录"
 if [ ! -d "logs" ]; then 
-    mkdir logs echo "创建logs目录成功" 
+    mkdir logs 
+    echo "创建logs目录成功" 
 fi
 
 echo "判断 logs/result.jtl 是否存在，存在则删除"
@@ -34,7 +35,13 @@ echo "备份当前执行记录"
 cp $WORKSPACE/logs/result.jtl $WORKSPACE/logs/${REPORT_TIMESTAMP}_${BUILD_NUMBER}.jtl
 
 echo "生成html格式报告"
+
+# if [ ! -d "logs" ]; then 
+#     mkdir logs 
+#     echo "创建logs目录成功" 
+# fi
+
 tmpl="$JMETER/extras/jmeter-results-detail-report_21.xsl" 
 source_result=$WORKSPACE/logs/${REPORT_TIMESTAMP}_${BUILD_NUMBER}.jtl 
-target_result=$WORKSPACE/logs/${JOB_NAME}${REPORT_TIMESTAMP}_${BUILD_NUMBER}.html 
+target_result=$WORKSPACE/logs/${JOB_NAME}_${REPORT_TIMESTAMP}_${BUILD_NUMBER}.html 
 xsltproc $tmpl $source_result > $target_result
